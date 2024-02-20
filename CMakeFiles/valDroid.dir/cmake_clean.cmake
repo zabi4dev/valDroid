1dd2e9771920905b56f7f62ec0e84e2b0c70098a
zabi4dev <zabi4dev@gmail.com>
2024-02-20 05:03:28 +0300

    first commit

---
+++ CMakeFiles/valDroid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/valDroid.dir/src/main.c.o"
+  "CMakeFiles/valDroid.dir/src/main.c.o.d"
+  "valDroid"
+  "valDroid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/valDroid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/valDroid.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for valDroid.
+# This may be replaced when dependencies are built.
